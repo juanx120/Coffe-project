bfe07f7fbb11ede7e30e3c559251542c00de846d
juanx120 <guerrerojuancho78@gmail.com>
2024-03-25 19:57:33 -0500

    [A] Nav-bar

--- src/assets/jsx/nav-bar.jsx
+++ src/assets/jsx/nav-bar.jsx
@@ -2,6 +2,22 @@
 
 export function NavBar (){
     return(
-            <div>hola mundo</div>
+        <nav className="primary-nav">
+            <div className="container flex justify-around">
+                <div className="primary-nav__left flex justify-around aling-center">
+                    <a href="">inicio</a>
+                    <a href="">herramientas</a>
+                </div>
+                <div className="primary-nav__center">
+                    <a href="/">
+                        <img src="./src/assets/images/de-la-mata-a-la-taza-trasparente.png" alt="" />
+                    </a>
+                </div>
+                <div className="primary-nav__right flex justify-around aling-center">
+                    <a href="">recorrido</a>
+                    <a href="">acerca del cafe</a>
+                </div>
+            </div>
+        </nav>
     )
 }
