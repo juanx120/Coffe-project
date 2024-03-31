33ed3af6286995d96d34870568b2ac1a2cec8382
juanx120 <guerrerojuancho78@gmail.com>
2024-03-30 22:41:39 -0500

    [A] Swiper library and modify single quad

--- src/assets/jsx/components/single-quad.jsx
+++ src/assets/jsx/components/single-quad.jsx
@@ -1,18 +1,25 @@
-export function SingleQuad ({id_item,bgcolor,title,srcImage,altImage,copy,btn,btn_label}){
-    return(
-        <section className={`single-quad bg-${bgcolor} flex aling-center`} id={id_item}>
+export function SingleQuad ({lista}){
+    const renderListItems = (items) => {
+        return items.map((item, index) =>  
+        <div key={index} className={`single-quad bg-${item.bg_color} flex aling-center`} id={index}>
             <div className="single-quad__container container flex aling-center justify-around">
                 <div className="single-quad__left">
                     <figure className="single-quad__left-image">
-                        <img src={srcImage} alt={altImage} />
+                        <img src={item.image} alt={item.alt_image} />
                     </figure>
                 </div>
                 <div className="single-quad__right flex flex-column justify-evenly">
-                    <h2 className="single-quad__right-title">{title}</h2>
-                    <p className={`single-quad__right-copy id_copy_${id_item}`}>{copy}</p>
-                    <a href={btn} className="btn">{btn_label}</a>
+                    <h2 className="single-quad__right-title">{item.title}</h2>
+                    <p className="single-quad__right-copy">{item.copy}</p>
+                    <a href={item.btn_href} className="btn">{item.btn_label}</a>
                 </div>
-            </div>
+            </div>)
+        </div>);
+    }
+
+    return(
+        <section className="single-quad-section">
+            {renderListItems(lista)}
         </section>
     )
 }
--- src/assets/jsx/index.jsx
+++ src/assets/jsx/index.jsx
@@ -4,11 +4,10 @@ import ReactDOM from 'react-dom/client'
 import data from '../data.json'
 import { NavBar } from './components/nav-bar'
 import { HeroImage } from './components/hero-image'
-import {SingleQuad } from './components/single-quad'
+import { SingleQuad } from './components/single-quad'
+import { NSwiper } from './components/swiper'
 
 console.log(data.index.intro_content.title);
-  let quadCount = 1;
-  
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
@@ -18,37 +17,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       srcImage={data.index.intro_content.image} 
       altImage={data.index.intro_content['alt-image']} 
     />
-    <SingleQuad 
-      id_item={quadCount} 
-      bgcolor={data.index[`quad_${quadCount}`]['bg-color']} 
-      title={data.index[`quad_${quadCount}`].title} 
-      srcImage={data.index[`quad_${quadCount}`].image} 
-      altImage={data.index[`quad_${quadCount}`].altImage} 
-      copy={data.index[`quad_${quadCount}`].copy} 
-      btn={data.index[`quad_${quadCount}`].btn_href} 
-      btn_label={data.index[`quad_${quadCount}`].btn_label}
+    <SingleQuad
+      lista={data.index['single-quad']}
     />
-    <div className='display-none'>{quadCount = quadCount + 1}</div>
-    <SingleQuad 
-      id_item={quadCount} 
-      bgcolor={data.index[`quad_${quadCount}`]['bg-color']} 
-      title={data.index[`quad_${quadCount}`].title} 
-      srcImage={data.index[`quad_${quadCount}`].image} 
-      altImage={data.index[`quad_${quadCount}`].altImage} 
-      copy={data.index[`quad_${quadCount}`].copy} 
-      btn={data.index[`quad_${quadCount}`].btn_href} 
-      btn_label={data.index[`quad_${quadCount}`].btn_label}
-    />
-    <div className='display-none'>{quadCount = quadCount + 1}</div>
-    <SingleQuad 
-      id_item={quadCount} 
-      bgcolor={data.index[`quad_${quadCount}`]['bg-color']} 
-      title={data.index[`quad_${quadCount}`].title} 
-      srcImage={data.index[`quad_${quadCount}`].image} 
-      altImage={data.index[`quad_${quadCount}`].altImage} 
-      copy={data.index[`quad_${quadCount}`].copy} 
-      btn={data.index[`quad_${quadCount}`].btn_href} 
-      btn_label={data.index[`quad_${quadCount}`].btn_label}
+    <NSwiper
     />
   </>
 );
